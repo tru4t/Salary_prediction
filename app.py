8e2b8a9cc528be52b4f0c4bf956d3809104b76a3
tru4t <it.ericjude.b17@gmail.com>
2026-01-18 13:46:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import pandas as pd
 import joblib
 
 model = joblib.load("Salary_prediction_model.pkl")
-encoders = joblib.load("label_encoders_sp.pkl")
+encoders = joblib.load("label_encoder_sp.pkl")
 
 st.title("salary Prediction App")
 
